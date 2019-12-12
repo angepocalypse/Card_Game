8462b84079c9ccc57510c5a738e02bdaa5d2db58
angepocalypse <tomjangel@yahoo.com>
2019-12-11 22:28:57 -0800

    Add PlayArea class

---
+++ PlayArea.py
@@ -0,0 +1,30 @@
+from Deck import Deck
+from Player import Player
+
+
+class PlayArea:
+    """A user's play area to put cards in play"""
+
+    def __init__(self, player, deck=None):
+        self.player = player
+        self.deck = deck
+
+    @property
+    def player(self):
+        return self.__player
+
+    @player.setter
+    def player(self, player):
+        if not isinstance(player, Player):
+            raise TypeError("PlayArea player must be Player object")
+        self.__player = player
+
+    @property
+    def deck(self):
+        return self.__deck
+
+    @deck.setter
+    def deck(self, deck):
+        if not isinstance(deck, Deck):
+            raise TypeError("PlayArea deck must be Deck object")
+        self.__deck = deck
