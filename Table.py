cc8e2bde8034903f41bb86013ef5f2093447b698
angepocalypse <tomjangel@yahoo.com>
2019-12-11 22:24:00 -0800

    Change Table class attributes to require player_one and player_two

--- Table.py
+++ Table.py
@@ -1,17 +1,32 @@
 from Player import Player
+from PlayArea import  PlayArea
 
 
 class Table:
-    """A user's table to put cards in play"""
-    def __init__(self, player):
-        self.player = player
+    """A table to put two Players"""
+
+    def __init__(self, player_one, player_two):
+        self.player_one = player_one
+        self.player_two = player_two
+        self.play_area1 = PlayArea(self.player_one)
+        self.play_area2 = PlayArea(self.player_two)
 
     @property
-    def player(self):
-        return self.__player
+    def player_one(self):
+        return self.__player_one
 
-    @player.setter
-    def player(self, player):
-        if not isinstance(player, Player):
+    @player_one.setter
+    def player_one(self, player_one):
+        if not isinstance(player_one, Player):
             raise TypeError("Table player must be Player object")
-        self.__player = player
+        self.__player_one = player_one
+    
+    @property
+    def player_two(self):
+        return self.__player_two
+
+    @player_two.setter
+    def player_two(self, player_two):
+        if not isinstance(player_two, Player):
+            raise TypeError("Table player must be Player object")
+        self.__player_two = player_two
